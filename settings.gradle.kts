6c3a934cd087f1ac26a13dc9e1f64692139c27a4
Jared Ornstead <jared@singular.net>
2024-02-24 15:02:39 -0800

    Initial Build

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven{ url = uri("https://maven.singular.net/") }
+    }
+}
+
+rootProject.name = "Android Kotlin Application"
+include(":app")
+ 
